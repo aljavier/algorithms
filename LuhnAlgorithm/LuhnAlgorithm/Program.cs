7230c6d62122f08f934702eb4bdb0c732ecd6280
aljavier <stackncode@gmail.com>
2017-04-12 11:17:48 -0400

    Luhn algorithm implementation example in C# added.

---
+++ LuhnAlgorithm/LuhnAlgorithm/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cardNumber = string.Empty;
+
+            while (cardNumber.ToUpper() != "QUIT")
+            {
+                try
+                {
+                    Console.Write("Enter credit card number (or quit, to exit) > ");
+
+                    cardNumber = Console.ReadLine().Trim();
+
+                    Console.WriteLine($"Is valid? {IsValidCardNumber(cardNumber)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\r\n" + ex.Message);
+                }
+
+            }
+        }
+
+        static bool IsValidCardNumber(string number)
+        {
+            bool even = true;
+            int check = 0;
+            char[] digits = number.ToCharArray();
+
+            for (int idx = digits.Length - 2; idx >= 0; idx--)
+            {
+                if (even)
+                {
+                    int value = 2 * int.Parse(digits[idx].ToString());
+
+                    value = (value > 9) ? (value - 9) : value;
+                    check += value;
+                }
+                else
+                {
+                    check += int.Parse(digits[idx].ToString());
+                }
+                even = !even;
+            }
+
+            check = (check * 9) % 10;
+
+            return (check == int.Parse(digits[digits.Length - 1].ToString()));
+        }
+    }
+}
---
+++ LuhnAlgorithm/README.md
@@ -0,0 +1,11 @@
+### Luhn Algorithm ###
+
+According to Wikipedia:
+
+    The Luhn algorithm or Luhn formula, also known as the "modulus 10" or "mod 10" algorithm, is a simple 
+    checksum formula used to validate a variety of identification numbers, such as credit card numbers, 
+    IMEI numbers, National Provider Identifier numbers in the US, Canadian Social Insurance Numbers, and 
+    Greek Social Security Numbers (ΑΜΚΑ). It was created by IBM scientist Hans Peter Luhn and described in 
+    U.S. Patent No. 2,950,048, filed on January 6, 1954, and granted on August 23, 1960.
+
+You can read more about it [here](https://en.wikipedia.org/wiki/Luhn_algorithm).
